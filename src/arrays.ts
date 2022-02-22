6a3d0037e85ab0331d700b0a74cd88528b8d1d75
Jackson Leadlove <leadlove@udel.edu>
2022-02-21 22:30:12 -0500

    Added solutions to first 7 functions

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,3 +1,6 @@
+import { parseConfigFileTextToJson } from "typescript";
+import { inflateSync } from "zlib";
+
 /**
  * Consume an array of numbers, and return a new array containing
  * JUST the first and last number. If there are no elements, return
@@ -5,7 +8,11 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    const endList: number[] = [numbers[0], numbers[numbers.length - 1]];
+    if (numbers.length === 0) {
+        return numbers;
+    }
+    return endList;
 }
 
 /**
@@ -13,7 +20,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((numbers: number): number => numbers * 3);
 }
 
 /**
@@ -21,7 +28,9 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map((string: string): number =>
+        isNaN(parseInt(string, 10)) ? 0 : parseInt(string, 10)
+    );
 }
 
 /**
@@ -32,7 +41,13 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    const noDollars = amounts.map((element: string): string =>
+        element.startsWith("$") ? element.slice(1, element.length) : element
+    );
+    const toNumbers = noDollars.map((element: string): number =>
+        isNaN(parseInt(element, 10)) ? 0 : parseInt(element, 10)
+    );
+    return toNumbers;
 };
 
 /**
@@ -41,7 +56,13 @@ export const removeDollars = (amounts: string[]): number[] => {
  * in question marks ("?").
  */
 export const shoutIfExclaiming = (messages: string[]): string[] => {
-    return [];
+    const shout = messages.map((message: string): string =>
+        message.endsWith("!") ? message.toUpperCase() : message
+    );
+    const questions = shout.filter(
+        (message: string): boolean => !message.endsWith("?")
+    );
+    return questions;
 };
 
 /**
@@ -49,7 +70,10 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const lessThanFour = words.filter(
+        (word: string): boolean => word.length < 4
+    );
+    return lessThanFour.length;
 }
 
 /**
@@ -58,7 +82,13 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    if (colors.length === 0) {
+        return true;
+    }
+    return colors.every(
+        (color: string): boolean =>
+            color === "red" || color === "blue" || color === "green"
+    );
 }
 
 /**
