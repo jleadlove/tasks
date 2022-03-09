2a31efb675fd1aaf5016cb38c3aad2c334df41ef
Jackson Leadlove <leadlove@udel.edu>
2022-03-09 18:26:46 -0500

    Completed CheckAnswer component

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { ChangeEventHandler, useState } from "react";
+import { Form } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, checkUserAnswer] = useState<string>("");
+    function checkMatching(event: ChangeEvent) {
+        checkUserAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="answerChecker">
+                <Form.Label>Check Answer:</Form.Label>
+                <Form.Control value={userAnswer} onChange={checkMatching} />
+            </Form.Group>
+            <div>{userAnswer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
