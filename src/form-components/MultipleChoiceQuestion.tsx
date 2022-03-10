907ee0b8b6fb30e629102fa1231326c37856116d
Jackson Leadlove <leadlove@udel.edu>
2022-03-09 23:05:54 -0500

    Completed MultipleChoiceQuestion component

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 export function MultipleChoiceQuestion({
     options,
     expectedAnswer
@@ -7,9 +10,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>(options[0]);
+    function updateUserAnswer(event: ChangeEvent) {
+        setUserAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <div>
+                <h3>Multiple Choice Question</h3>
+                <Form.Group controlId="multChoiceQuestion">
+                    <Form.Select value={userAnswer} onChange={updateUserAnswer}>
+                        {options.map((answerOption: string) => (
+                            <option key={answerOption} value={answerOption}>
+                                {answerOption}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+            </div>
+            <div>{userAnswer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
