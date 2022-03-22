8a5ad224628bc1b605ffdc51d60e0347229934cc
Jackson Leadlove <leadlove@udel.edu>
2022-03-22 13:27:26 -0400

    Initial commit

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { Question, QuestionType } from "../interfaces/question";
+import { SIMPLE_QUESTIONS_2 } from "../data/questions.json";
 
 export function Quizzer(): JSX.Element {
     return <h3>Quizzer</h3>;
